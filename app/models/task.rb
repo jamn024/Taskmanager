0d2018a900dcbc6413020173f808397e33fb26c4
adelaossa <andres@comercializadora-s3.com>
2015-02-21 20:51:47 +0000

    add validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+    validates :title, presence: true, length: { maximum: 6 }
+    validates :user_id, presence:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    validates :nombre, presence: true
+    validates :apellido, presence: true
+    validates :correo, presence: true
 end
